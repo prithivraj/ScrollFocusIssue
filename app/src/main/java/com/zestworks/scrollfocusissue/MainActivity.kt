1bd984c371e1df5fff38468767f573c5248dc83a
prithivraj <prithivraj007@gmail.com>
2022-01-18 00:37:34 +0000

    Initial commit

---
+++ app/src/main/java/com/zestworks/scrollfocusissue/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.zestworks.scrollfocusissue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import com.google.accompanist.insets.statusBarsPadding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProvideWindowInsets {
+                Scaffold(
+                    Modifier
+                        .navigationBarsWithImePadding()
+                        .statusBarsPadding()
+                ) {
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                            .padding(16.dp)
+                    ) {
+                        repeat(20) {
+                            Spacer(modifier = Modifier.height(40.dp))
+                            Text(text = "Enter text")
+                            CustomTextField(
+                                textFieldValue = remember {
+                                    mutableStateOf(
+                                        TextFieldValue()
+                                    )
+                                }, onValueChange = {
+                                })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    textFieldValue: MutableState<TextFieldValue>,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
+    onValueChange: (TextFieldValue) -> Unit = {},
+) {
+    val relocationRequester = remember { BringIntoViewRequester() }
+    val interactionSource = remember { MutableInteractionSource() }
+    val interactionSourceState = interactionSource.collectIsFocusedAsState()
+    val scope = rememberCoroutineScope()
+    val ime = LocalWindowInsets.current.ime
+
+    LaunchedEffect(ime.isVisible, interactionSourceState.value) {
+        if (ime.isVisible && interactionSourceState.value) {
+            scope.launch {
+                delay(300)
+                relocationRequester.bringIntoView()
+            }
+        }
+    }
+
+    val focusRequester = FocusRequester()
+    val isFocused = remember { mutableStateOf(false) }
+
+    TextField(
+        modifier = modifier
+            .focusRequester(focusRequester)
+            .bringIntoViewRequester(relocationRequester)
+            .onFocusChanged {
+                isFocused.value = it.isFocused
+            }
+            .fillMaxWidth(),
+        interactionSource = interactionSource,
+        value = textFieldValue.value,
+        onValueChange = {
+            onValueChange(it)
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = imeAction
+        ),
+
+        )
+}
